d852b970b4f4fd4ef4c04038087bfa532a31bebf
Kraken0403 <trivedibhavya1997@gmail.com>
2025-01-27 01:58:42 +0530

    yes

--- components_bkp/filter.js
+++
@@ -1,6 +0,0 @@
-// filters.js
-export const filterProducts = (products, selectedCategory) => {
-    if (!selectedCategory) return products; // All products if no filter selected
-  
-    return products.filter(product => product.category === selectedCategory);
-  };
